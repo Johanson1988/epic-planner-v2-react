5b42d7b45910fc7a6a394f092db1f79c02ef95ec
Johann <johanson88@gmail.com>
2020-03-17 20:14:51 +0100

    add footer

--- src/components/DisplayEvents.js
+++ src/components/DisplayEvents.js
@@ -1,17 +1,11 @@
 import React, {Component} from 'react';
-
-
-import Footer from './../components/Footer';
-import DisplayEvents from './'
+import {Link} from 'react-router-dom';
 
 export default class MyDayplans extends Component {
     render() {
         return(
             <div>
-                <h1>My Dayplans</h1>
-                <h2>Epic!</h2>
-                <Footer />
-
+                <h1>Display Events</h1>
             </div>
         )
     }
---
+++ src/pages/CreateDayplan.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+export default class CreateDayplan extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Navbar</h1>
+                <h2>Epic!</h2>
+                
+            </div>
+        )
+    }
+}
