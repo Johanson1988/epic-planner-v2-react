cb0b972a46138b9e8dfc683a48a81ff3350e07aa
nurlopez <nlopezcolom@gmail.com>
2020-03-17 21:11:09 +0100

    Create: login + sign up forms in pages

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,58 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+// import { withAuth } from "../lib/AuthProvider";
+// import Footer from "../components/Footer";
 
 export default class Login extends Component {
-    render() {
-        return(
-            <div>
-                <h1>Login</h1>
-                <h2>Epic!</h2>
-            </div>
-        )
-    }
+  state = {
+    username: "",
+    password: ""
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    const { username, password } = this.state;
+    this.props.login({
+      username,
+      password
+    });
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+  render() {
+    const { email, password } = this.state;
+    return (
+      <div>
+        <h1> Login </h1>{" "}
+        <form onSubmit={this.handleFormSubmit}>
+          <div>
+            <label> Email: </label>{" "}
+            <input
+              type="text"
+              name="email"
+              value={email}
+              onChange={this.handleChange}
+            />{" "}
+          </div>{" "}
+          <div>
+            <label> Password: </label>{" "}
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={this.handleChange}
+            />
+          </div>
+          <input type="submit" value="Login" />
+        </form>
+        <p> Don't have an account?</p>
+        <Link to={"/signup"}>Signup </Link>
+      </div>
+    );
+  }
 }
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,14 +1,97 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+// import { withAuth } from "../lib/AuthProvider";
+// import Footer from "../components/Footer";
+
 
 export default class Signup extends Component {
+    state = {
+        fullName: "",
+        email:"",
+        password:"",
+        location:"",
+        keywords:""
+    };
+    
+    handleFormSubmit = event => {
+        event.preventDefault();
+        const { fullName, email, password, location, keywords } = this.state;
+        this.props.signup( { fullName, email, password, location, keywords } ); // props.signup is Provided by withAuth() and Context API
+      };
+    
+      handleChange = event => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+      };
+    
+    
     render() {
+        const { fullName, email, password, location, keywords } = this.state;
         return(
             <div>
                 <h1>SIGNUP</h1>
-                <h2>Epic!</h2>
-                
-            </div>
+
+        <form onSubmit={this.handleFormSubmit}>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="fullname"
+            value={fullName}
+            onChange={this.handleChange}
+          />
+          </div>
+
+          <div>
+          <label>Email:</label>
+          <input
+            type="text"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+          />
+          </div>
+
+          <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+          />
+          </div>
+{/*     */}
+          <div>
+          <label>Location:</label>
+               <select id="location" onChange={this.handleChange} value={this.state.value}>
+                  <option value="Sants">Sants</option>
+                  <option value="Barceloneta">Barceloneta</option>
+                  <option value="Poble Nou">Poble Nou</option>
+               </select>
+               <p></p>
+               <p>{this.state.value}</p>
+           </div>
+       
+        </form>
+      
+        <div>
+          <label>Keywords:</label>
+          <input
+            type="keywords"
+            name="keywords"
+            value={keywords}
+            onChange={this.handleChange}
+          />
+          </div>
+
+        <p>Already have account?</p>
+        <Link to={'/login'}> Login</Link>
+
+        </div>
         )
     }
 }
+
+
+
